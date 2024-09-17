44d53af7fd0ae89c066aef4704837b3318d62caa
Karel <karelh2207@gmail.com>
2024-09-17 15:55:55 -0300

    actualizando

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Para asegurarse de que las rutas relativas funcionen
+  build: {
+    outDir: 'dist', // Ruta donde se genera el build
+  },
 })
