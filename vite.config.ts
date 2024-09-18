2053e67dc64af4d54b5723d18fadb86472c45b68
Karel <karelh2207@gmail.com>
2024-09-18 11:50:06 -0300

    actualizando

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Para asegurarse de que las rutas relativas funcionen
-  build: {
-    outDir: 'dist', // Ruta donde se genera el build
-  },
+  base: './https://karel95.github.io/karldev95', // Para asegurarse de que las rutas relativas funcionen
 })
